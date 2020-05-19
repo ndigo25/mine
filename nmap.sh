7b1729a45cca92a43efc900f0d8bcf33f394b6f3
nk3sec <30809260+nk3sec@users.noreply.github.com>
2020-05-19 19:35:08 +0000

    Update nmap.sh

--- nmap.sh
+++ nmap.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-echo "enter your address:"
+echo -n "enter your address: "
 read ADDRESS
 for ip in $ADDRESS; do
 nmap -sC -sT -v -p- -oA nmap $address$ip
-trap exit INT
+echo -n "Here are your results. Would you like me to move them to a different directory? (Y/n): "
+read RESULTS
+if [[ $RESULTS =~ ^[Yy]$ ]]
+then
+echo -n "Where would you like me to move them?: "
+read DIRECTORY
+mv nmap.nmap nmap.gnmap nmap.xml $DIRECTORY
+echo "Moved your results to" $DIRECTORY
+if [[ $RESULTS =~ ^[Nn]$ ]]
+then
+exit 1
+fi
+fi
 done
